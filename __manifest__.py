f0e811b0b15ede896da800ffc51f25308b757063
Bisiach Lucio <34654675+LucioBisiach@users.noreply.github.com>
2022-07-09 16:03:28 -0300

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Is Customer or Vendor',
+    'version': '15.0',
+    'summary': """Is Customer or Vendor""",
+    'description': 'The booleans of "Is Customer" and "Is Vendor" that were removed from Odoo 13 are added',
+    "category": "Partner",
+    'author': 'Bisiach Lucio',
+    'company': 'Bisiach Lucio',
+    'website': "",
+    'depends': ['l10n_ar'],
+    'data': [
+        'views/res_partner.xml'
+    ],
+    'images': [],
+    'license': 'AGPL-3',
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
